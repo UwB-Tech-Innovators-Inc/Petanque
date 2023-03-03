4b7078963c2cfdf1c1e6cf2cb303c619776cda18
szymqn <koodziejskisz@outlook.com>
2023-03-03 20:57:11 +0100

    update client

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ import pymongo
 from django.shortcuts import render
 
 # Create your views here.
-client = pymongo.MongoClient("mongodb+srv://admin:1234@petanque.gomnjmu.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://admin:1234@cluster0.tettcne.mongodb.net/?retryWrites=true&w=majority")
 mydb = client['sample_analytics']
 mycol = mydb['customers']
 
