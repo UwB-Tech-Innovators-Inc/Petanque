d8ec0c973300de035aec6a8c08325c5a63cd56f3
szymqn <koodziejskisz@outlook.com>
2023-03-05 13:29:05 +0100

    config rest_framework

--- Petanque/settings.py
+++ Petanque/settings.py
@@ -40,8 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'base.apps.BaseConfig',
+
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
