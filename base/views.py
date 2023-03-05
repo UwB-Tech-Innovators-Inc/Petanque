12912d907c137148144c387c0aa6c0431b4476e2
szymqn <koodziejskisz@outlook.com>
2023-03-05 13:29:35 +0100

    create Player model

--- base/views.py
+++ base/views.py
@@ -2,10 +2,3 @@ import pymongo
 from django.shortcuts import render
 
 # Create your views here.
-client = pymongo.MongoClient("mongodb+srv://admin:1234@cluster0.tettcne.mongodb.net/?retryWrites=true&w=majority")
-mydb = client['sample_analytics']
-mycol = mydb['customers']
-
-x = mycol.find_one()
-
-print(x)
