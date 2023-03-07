7da010725a3916b894c8c9f5c3a68eab5d590433
szymqn <koodziejskisz@outlook.com>
2023-03-08 00:14:20 +0100

    create playerView sample

--- Petanque/settings.py
+++ Petanque/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'base.apps.BaseConfig',
 
 ]
@@ -130,9 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-]
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
