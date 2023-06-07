ee73a56e939a714d8c9f8c7f5db8893b089305ae
Cezary Androsiuk <czandros@wp.pl>
2023-06-07 21:14:52 +0200

    clean up and prepare for sorting by other conditions

--- algorythm.py
+++ algorythm.py
@@ -63,57 +63,37 @@ def phase_1_prepare_result_json(array):
     pass
 
 
-# def check_conflicts(teans):
-#     ################################################## ASSUME THAT THERE ARE ONLY ONE CONFLICT ####################
-#     # count number of winns
-#     print(teams)
-#     winsCount = {}
-#     for team_name in teams:
-#         if team[team_name] in winsCount:
-#             winsCount[team[team_name]] += 1
-#         else:
-#             winsCount[team[team_name]] = 1
-#
-#     conflict = 0
-#     additional_match = 0
-#
-#     # check if group have equal wins count
-#     for i in winsCount:
-#         if winsCount[i] == 2:
-#             additional_match += 1
-#     if additional_match > 1:
-#         print("są 2 drużyny posiadające identyczną ilość wygranych: ", additional_match)
-#         return 2
-#
-#     for i in winsCount:
-#         if winsCount[i] >= 3:
-#             conflict += 1
-#     if winsCount > 1:
-#         print("są ponad 3 drużyny posiadające identyczną ilość wygranych: ", winsCount)
-#         return 3
-#
-#     return 0
+def solve_intrested_teams_conflicts(intrested_teams, group):
+    conflict_solved_array = []
+    for i in intrested_teams:
+        conflict_solved_array.append(i)
+    conflict_solved_array.reverse()
+    return conflict_solved_array
 
-
-def sort_by_wins_count(teams):
+def sort_by_win_count(teams):
     sorted_teams = [[]]
     for i in range(len(teams)):
         sorted_teams.append([])
         for team_name in teams:
             if teams[team_name] == i:
                 sorted_teams[i].append(team_name)
+    sorted_teams.reverse()
     return sorted_teams
 
-def single_group_conditions(group):
+def single_group_order(group):
     teams = {}
+    # build all teams array
     for team in group["teams"]:
         team_name = group["teams"][team]
         teams[team_name] = 0  # add new    key: value
 
-    for round_name in group["games"]:
-        for game_name in group["games"][round_name]:
-            game = group["games"][round_name][game_name]
-            # print(game)
+    games = group["games"]
+    for round_name in games:
+        round = games[round_name]
+        for game_name in round:
+            game = round[game_name]
+
+            # check which team won the match and add to win counter: (dict in teams)
             if game["result_1"] > game["result_2"]:
                 teams[game["team_1"]] += 1
             elif game["result_1"] < game["result_2"]:
@@ -122,42 +102,37 @@ def single_group_conditions(group):
                 print("nie powinno być identycznych wyników meczu")
                 exit(1)
 
-
+    # testing what happens if someone have equals wins count
     if 'G_1_1' in teams:
-        teams['G_1_1'] = 2
+        teams['G_1_1'] = 1
+    if 'G_1_2' in teams:
+        teams['G_1_2'] = 1
 
-    teams_sorted_by_values = sort_by_wins_count(teams)
+    print(teams)
 
-    for i in teams_sorted_by_values:
-        if len(i) == 2:
-            print("do obsłużenia gdy 2 drużyny mają identyczną sumę zwycięstw")
-            print(i)
-        if len(i) >= 3:
-            # function (i, array)
-            print(i)
+    teams_sorted_by_win_count = sort_by_win_count(teams) #  [[x win], [x-1 win], [x-2 win], ..., [1 win], [0 win]]
 
+    # check if any additional matches are needed ( 3b bezpośredni mecz)
+    additional_maches_count = 0
+    for teams_array in teams_sorted_by_win_count:
+        if len(teams_array) == 2:
+            additional_maches_count += 1
+    if additional_maches_count > 0:
+        print("jest przynajmniej jedna grupa składająca się z drużyn posiadajacych identyczną ilość wygranych!")
+        exit(1)
 
-    return 1
-    # conflict = check_conflicts(teams)
-    #
-    # if conflict == 0:
-    #     # sort by the highest win values
-    #     sorted_teams = dict(sorted(teams.items(), key=lambda x: x[1], reverse=True))
-    #
-    #     # # return decreasing ordered dictionary with sums of wins
-    #     # return sorted_teams
-    #
-    #     # return names in decreasing ordered array
-    #     array_of_team_names = []
-    #     for team_name in sorted_teams:
-    #         array_of_team_names.append(team_name)
-    #     return array_of_team_names
-    # elif conflict == 2:
-    #     # there are 2 equal win sums
-    #     pass
-    # elif conflict == 3:
-    #     # there are more than 2 equal win sums
-    #     pass
+    teams_sorted = []
+    # solve conflicts 
+    for teams_array in teams_sorted_by_win_count:
+        if len(teams_array) >= 3:
+            conflict_solved_team_array = solve_intrested_teams_conflicts(teams_array, group)
+            for team in conflict_solved_team_array:
+                teams_sorted.append(team)
+        elif len(teams_array) != 0:
+            teams_sorted.append(teams_array[0])
+
+    return teams_sorted
+
 
 
 def phase_1_result(json):
@@ -173,7 +148,10 @@ def phase_1_result(json):
             continue
 
         group = json[group_name]
-        array_of_team_names = single_group_conditions(group)
+        array_of_team_names = single_group_order(group)
+        print(array_of_team_names)
+        break # to iterate over one group
+
         # print(array_of_team_names)
         # print()
     #     group_result =
@@ -198,35 +176,5 @@ def tmp():
     data = json.load(file)
     file.close()
     phase_1_result(data)
-
-    # pass
-    #
-    # d = {'G_7_1': 3, 'G_7_2': 2, 'G_7_3': 1, 'G_7_4': 0, 'G_7_x': 2}
-    # a = {}
-    # print(d)
-    # for d_name in d:
-    #     if d[d_name] in a:
-    #         a[d[d_name]] += 1
-    #     else:
-    #         a[d[d_name]] = 1
-    #     print(d[d_name], a)
-    #
-    # # a[3] = 4
-    # print(a)
-
-    pass
-
-    # kontakty = {
-    #     "Jan": {"numer1": 111111111, "numer2": 222222222, "numer3": 333333333},
-    #     "Jacek": {"numer1": 444444444, "numer2": 555555555, "numer3": 666666666},
-    #     "Janusz": {"numer1": 777777777, "numer2": 888888888, "numer3": 999999999}
-    # }
-    #
-    # print()
-    # for imie in kontakty:
-    #     print(imie, " ma numery telefonu", end=": ")
-    #     for numer in kontakty[imie]:
-    #         print(kontakty[imie][numer], end=", ")
-    #     print()
 
 tmp()
